169dce4c926fc9fea06ae1f2abdbb495b7f5fdea
racosta310512 <rafael.leidis@gmail.com>
2025-06-04 17:58:16 -0300

    ,

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,14 +1,16 @@
 // src/services/productService.js
-const API_URL = import.meta.env.VITE_API_URL + '/products';
+const API_URL = import.meta.env.VITE_API_URL; // https://jiga-store.vercel.app
 
 export const getAllProducts = async () => {
   const res = await fetch(API_URL);
+  if (!res.ok) throw new Error('Error al obtener productos');
   const data = await res.json();
   return data;
 };
 
 export const getProductById = async (id) => {
   const res = await fetch(`${API_URL}/${id}`);
+  if (!res.ok) throw new Error('Error al obtener producto');
   const data = await res.json();
   return data;
 };
